23079255775688b2579229dcd0516a04998185a9
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2005-12-02 18:33:20 +0000

    Fixed Bottom Of Page linking

--- trunk/themes/vanilla/templates/head.php
+++ trunk/themes/vanilla/templates/head.php
@@ -1,38 +1,34 @@
 <?php
 // Note: This file is included from the library/Utility.Control.Head.php class.
 
-echo("<".chr(63)."xml version=\"1.0\" encoding=\"utf-8\"".chr(63).">
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-<base href=\"".$this->Context->Configuration["REWRITE_BASE_URL"]."\" />
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en-ca\">
-   <head>");
-      $this->CallDelegate("PreTitleRender");
+$HeadString = "<".chr(63)."xml version=\"1.0\" encoding=\"utf-8\"".chr(63).">
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
+// <base href=\"".$this->Context->Configuration["REWRITE_BASE_URL"]."\" />
+$HeadString .= "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en-ca\">
+   <head>";
       echo("<title>".$this->Context->Configuration["APPLICATION_TITLE"]." - ".$this->Context->PageTitle."</title>
       <link rel=\"shortcut icon\" href=\"/favicon.ico\" />");
-      $this->CallDelegate("PreStylesheetsRender");
       if (is_array($this->StyleSheets)) {
          $StyleSheetCount = count($this->StyleSheets);
          $i = 0;
          for ($i = 0; $i < $StyleSheetCount; $i++) {
-            echo("\r\n<link rel=\"stylesheet\" type=\"text/css\" href=\"".$this->StyleSheets[$i]["Sheet"]."\"".($this->StyleSheets[$i]["Media"] == ""?"":" media=\"".$this->StyleSheets[$i]["Media"]."\"")." />");
+            $HeadString .= "\r\n<link rel=\"stylesheet\" type=\"text/css\" href=\"".$this->StyleSheets[$i]["Sheet"]."\"".($this->StyleSheets[$i]["Media"] == ""?"":" media=\"".$this->StyleSheets[$i]["Media"]."\"")." />";
          }
       }
-      $this->CallDelegate("PreScriptsRender");
       if (is_array($this->Scripts)) {
          $ScriptCount = count($this->Scripts);
          $i = 0;
          for ($i = 0; $i < $ScriptCount; $i++) {
-            echo("\r\n<script type=\"text/javascript\" src=\"".$this->Scripts[$i]."\"></script>");
+            $HeadString .= "\r\n<script type=\"text/javascript\" src=\"".$this->Scripts[$i]."\"></script>";
          }
       }
       if (is_array($this->Strings)) {
          $StringCount = count($this->Strings);
          $i = 0;
          for ($i = 0; $i < $StringCount; $i++) {
-            echo($this->Strings[$i]);
+            $HeadString .= $this->Strings[$i];
          }
       }
-      $this->CallDelegate("PreBodyRender");
-echo("</head>
+echo($HeadString . "</head>
    <body".$this->Context->BodyAttributes.">");
 ?>
