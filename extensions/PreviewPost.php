786b89bfe42fd0da0db49577b7e1ac1869f1ac0f
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2005-12-04 19:17:34 +0000

    Updated PreviewPost extension

--- extensions/PreviewPost.php
+++ extensions/PreviewPost.php
@@ -5,7 +5,7 @@ Extension Url: mailto:sirnot@gmail.com
 Description: Allows users to view a preview of what their post will look like.
 Version: 1.2
 Author: SirNot
-Author Url: mailto:sirnot@gmail.com
+Author Url: N/A
 */
 
 if(realpath($_SERVER['SCRIPT_FILENAME']) == realpath(__FILE__) && !defined('EXTENSION_IN_PREVIEW')) //a preview?
@@ -21,37 +21,58 @@ if(realpath($_SERVER['SCRIPT_FILENAME']) == realpath(__FILE__) && !defined('EXTE
 		include('../conf/settings.php');
 		include('../appg/init_ajax.php');
 		
+		class EmulatedUserInfoClass
+		{
+			var $Context, $AuthUserID, $AuthUsername;
+			
+			function EmulatedUserInfoClass(&$Context)
+			{
+				$this->Context = &$Context;
+				$this->AuthUserID = $this->Context->Session->User->UserID;
+				$this->AuthUsername = $this->Context->Session->User->Name;
+			}
+		}
+		
+		$EmulatedUserInfo = new EmulatedUserInfoClass($Context);
+		
 		//TODO: either emulate or use the Comment class instead of passing 0
 		if(get_magic_quotes_gpc()) $Text = stripslashes($Text);
-		$Text = $Context->StringManipulator->Parse($Text, 0, $Type, FORMAT_STRING_FOR_DISPLAY);
-		if($Type == 'Text') $Text = str_replace(array("\r", "\n", "\r\n"), '<br>', $Text);
+		$Text = $Context->StringManipulator->Parse($Text, $EmulatedUserInfo, $Type, FORMAT_STRING_FOR_DISPLAY);
+		if($Type == 'Text') $Text = str_replace(array("\r\n", "\r", "\n"), '<br>', $Text);
 		
 		echo($Text);
 	}
 }
 else if(in_array($Context->SelfUrl, array('post.php', 'comments.php')))
 {
+	//print_r($Context);
 	$Context->Dictionary['PostPreview'] = 'Post Preview';
 	
 	$Head->AddScript('./extensions/PreviewPost/preview.js');
 	$Head->AddStyleSheet('./extensions/PreviewPost/preview.css');
 	
-	$Context->AddToDelegate('DiscussionForm', 'DiscussionForm_PostSubmitRender', 'PostForm_PreviewPostButton');
+	$Context->AddToDelegate('DiscussionForm', 'DiscussionForm_PostSubmitRender', 'DiscussionForm_PreviewPostButton');
 	$Context->AddToDelegate('DiscussionForm', 'DiscussionForm_PostButtonsRender', 'PostForm_PreviewPostField');
-	$Context->AddToDelegate('DiscussionForm', 'CommentForm_PostSubmitRender', 'PostForm_PreviewPostButton');
+	$Context->AddToDelegate('DiscussionForm', 'CommentForm_PostSubmitRender', 'CommentForm_PreviewPostButton');
 	$Context->AddToDelegate('DiscussionForm', 'CommentForm_PostButtonsRender', 'PostForm_PreviewPostField');
 	
-	function PostForm_PreviewPostButton(&$DiscussionForm)
+	function DiscussionForm_PreviewPostButton(&$DiscussionForm)
 	{
-		global $Context;
-		
 		echo(
 			'<input name="btnPreview" value="Preview" class="Button SubmitButton" type="button" onclick="ShowPreview(\'frmPost'.
-			($Context->SelfUrl == 'comments.php' ? 'Comment' : 'Discussion').'\', \'./extensions/'.basename(__FILE__).'\')" />'
+			'Discussion\', \'./extensions/'.basename(__FILE__).'\')" />'
 		);
 	}
 	
-	function PostForm_PreviewPostField(&$DiscussionForm)
+	function CommentForm_PreviewPostButton(&$CommentForm)
+	{
+		echo(
+			'<input name="btnPreview" value="Preview" class="Button SubmitButton" type="button" onclick="ShowPreview(\'frmPost'.
+			'Comment\', \'./extensions/'.basename(__FILE__).'\')" />'
+		);
+	}
+	
+	function PostForm_PreviewPostField(&$Form)
 	{
 		global $Context;
 		
