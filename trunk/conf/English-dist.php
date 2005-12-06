2beb835d74fbfe31026e5514f0b1176e9aeab274
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2005-12-06 17:30:26 +0000

    Making it so updates don't cause conflicts in the conf directory

---
+++ trunk/conf/English-dist.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Note: this is a "-dist" file. You must create a copy of this file without the
+ "-dist" in the filename for your installation.
+ 
+ Some extensions will have custom language definitions.
+ You should enter any custom language definitions for an extension here.
+*/
+
+?>
--- trunk/conf/settings-dist.php
+++ trunk/conf/settings-dist.php
@@ -1,16 +1,20 @@
 <?php
 /*
-* Copyright 2003 - 2005 Mark O'Sullivan
-* This file is part of Vanilla.
-* Vanilla is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-* Vanilla is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-* The latest source code for Vanilla is available at www.lussumo.com
-* Contact Mark O'Sullivan at mark [at] lussumo [dot] com
-*
-* Description: Create a copy of this file and name it settings.php. Use that file to specify your own
-* custom information. DO NOT ADD your settings.php file to the repository or others will see your db
-* password (and other sensitive information).
+ Note: this is a "-dist" file. You must create a copy of this file without the
+ "-dist" in the filename for your installation.
+ 
+ Copyright 2003 - 2005 Mark O'Sullivan
+ This file is part of Vanilla.
+ Vanilla is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ Vanilla is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ The latest source code for Vanilla is available at www.lussumo.com
+ Contact Mark O'Sullivan at mark [at] lussumo [dot] com
+
+ Description: Create a copy of this file and name it settings.php. Use that file
+ to specify your own custom information. DO NOT ADD your settings.php file to
+ the repository or others will see your db password (and other sensitive
+ information).
 */
 
 // Database Settings
