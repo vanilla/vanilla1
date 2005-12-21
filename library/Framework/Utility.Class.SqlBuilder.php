a9652e8880f8b261430988e426873e12a0d9f7f8
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2005-12-21 22:25:53 +0000

    Restructuring libraries

---
+++ library/Framework/Utility.Class.SqlBuilder.php
@@ -0,0 +1,221 @@
+<?php
+/*
+* Copyright 2003 - 2005 Mark O'Sullivan
+* This file is part of Lussumo's Software Library.
+* Lussumo's Software Library is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+* Lussumo's Software Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* The latest source code is available at www.lussumo.com
+* Contact Mark O'Sullivan at mark [at] lussumo [dot] com
+* 
+* DESCRIPTION: Class that builds a string of sql to be executed
+* Applications utilizing this file: Vanilla;
+*/
+class SqlBuilder {
+	var $Fields;		// String of select fields
+	var $FieldValues;	// Array of field name/value pairs for inserting/updating
+	var $MainTable;		// Associative array with information about the main table in the statement
+	var $Joins;			// String of join clauses
+	var $Wheres;		// String of where clauses
+	var $GroupBys;		// String of group by fields
+	var $OrderBys;		// String of order by clauses
+	var $Limit;			// Limit for a select
+	var $Name;			// The name of this class
+	var $TablePrefix;	// Prefix all tables with this string
+   var $Context;
+	
+	
+	function AddFieldNameValue($FieldName, $FieldValue = "", $QuoteValue = 1, $Function = "") {
+		if ($QuoteValue) $FieldValue = "'".$FieldValue."'";
+		if ($Function != "") $FieldValue = $Function."(".$FieldValue.")";
+		$this->FieldValues[$FieldName] = $FieldValue;
+	}
+	
+	function AddGroupBy($Field, $TableAlias = "") {
+		if (is_array($Field)) {
+			$FieldCount = count($Field);
+			$i = 0;
+			for ($i = 0; $i < $FieldCount; $i++) {
+				$this->AddGroupBy($Field[$i],$TableAlias);
+			}
+		} else {
+			if ($Field != "") {
+				if ($TableAlias != "") $Field = $TableAlias.".".$Field;
+				if ($this->GroupBys != "") $this->GroupBys .= ", ";
+				$this->GroupBys .= $Field;
+			}
+		}
+	}
+	
+	// Adds a table to the join clause
+	function AddJoin($NewTable, $NewTableAlias, $NewTableField, $ExistingAlias, $ExistingField, $JoinMethod) {
+		$this->Joins .= $JoinMethod." ".$this->TablePrefix.$NewTable." ".$NewTableAlias
+			." on ".$ExistingAlias.".".$ExistingField." = ".$NewTableAlias.".".$NewTableField." ";
+	}
+	
+	function AddLimit($Index, $Length) {
+		$this->Limit = " limit $Index, $Length";
+	}
+	
+	function AddOrderBy($FieldName, $TableAlias = "", $SortDirection = "asc") {
+		if ($this->OrderBys != "") $this->OrderBys .= ", ";
+		if ($TableAlias != "") $FieldName = $TableAlias.".".$FieldName;
+		$this->OrderBys .= " ".$FieldName." ".$SortDirection;
+	}
+	
+	// $Field == the field to select (or fields if you supply an array)
+	// $TableAlias == the alias of the table to select the field from
+	// $FieldAlias == the alternate name for a field (ie. select field as blah) - ignored if you supply an array for $Field
+	function AddSelect($Field, $TableAlias = "", $FieldAlias = "", $Function = "", $FunctionParameters = "", $GroupByThisField = "0") {
+		if (is_array($Field)) {
+			$FieldCount = count($Field);
+			$i = 0;
+			for ($i = 0; $i < $FieldCount; $i++) {
+				$this->AddSelect($Field[$i],$TableAlias, "", "", "", $GroupByThisField);
+			}
+		} else {
+			if ($Field != "") {
+				// $GroupByThisField = ForceBool($GroupByThisField, 0);
+				if ($GroupByThisField) {
+					if ($this->GroupBys != "") $this->GroupBys .= ", ";
+					$this->GroupBys .= ($TableAlias != "" ? $TableAlias.".".$Field : $Field);
+					// $this->AddGroupBy($Field, $TableAlias);
+				}
+				if ($TableAlias != "") $Field = $TableAlias.".".$Field;
+				if ($Function != "" && $FunctionParameters == "") $Field = $Function."(".$Field.")";
+				if ($Function != "" && $FunctionParameters != "") $Field = $Function."(".$Field.", ".$FunctionParameters.")";
+				if ($this->Fields != "") $this->Fields .= ", ";
+				$this->Fields .= $Field;
+				if ($FieldAlias != "") $this->Fields .= " as ".$FieldAlias;
+			}
+		}
+	}
+	
+	// $Parameter1 == the first field in the comparison operation
+	// $Parameter2 == the second field in the comparison operation
+	// $Comparison operator == "=,>,<,in,<>,like" etc
+	// $AppendMethod == the method by which this where should be attached to existing wheres
+	function AddWhere($Parameter1, $Parameter2, $ComparisonOperator, $AppendMethod = "and", $Function = "", $QuoteParameter2 = "1", $StartWhereGroup = "0") {
+		$StartWhereGroup = ForceBool($StartWhereGroup, 0);
+
+		// Add the append method if there is an existing clause
+		if (!empty($this->Wheres) && substr($this->Wheres,strlen($this->Wheres)-1) != "(") {
+			$this->Wheres .= $AppendMethod." ";
+		}
+		if ($StartWhereGroup) $this->Wheres .= "(";
+		if ($QuoteParameter2 == '1') $Parameter2 = "'".$Parameter2."'";
+		if ($Function != "") $Parameter2 = $Function."(".$Parameter2.")";
+		
+		// Do the comparison operation
+		$this->Wheres .= $Parameter1." ".$ComparisonOperator." ".$Parameter2." ";
+	}
+	
+	function Clear() {
+		$this->Fields = "";
+		$this->FieldValues = array();
+		$this->MainTable = array();
+		$this->Joins = "";
+		$this->Wheres = "";
+		$this->GroupBys = "";
+		$this->OrderBys = "";
+		$this->Limit = "";
+		$this->Name = "SqlBuilder";
+		$this->TablePrefix = $this->Context->Configuration["DATABASE_TABLE_PREFIX"];
+	}
+	
+	function EndWhereGroup() {
+		$this->Wheres .= ") ";
+	}
+	
+	// Returns a delete statement
+	function GetDelete() {
+		$sReturn = "delete ";
+		$sReturn .= "from ".$this->MainTable["TableName"]." ";
+		if ($this->Wheres != "") $sReturn .= " where ".$this->Wheres." ";
+		$this->WriteDebug($sReturn);
+		return $sReturn;
+	}
+
+	// Returns an insert statement
+	function GetInsert($UseIgnore = "0") {
+		$sReturn = "insert ";
+		if ($UseIgnore == "1") $sReturn .= "ignore ";
+		$sReturn .= "into ";
+		$sReturn .= $this->MainTable["TableName"]." ";
+		$Fields = "";
+		$Values = "";
+		while (list($name, $value) = each($this->FieldValues)) {
+			if ($Fields != "") {
+				$Fields .= ", ";
+				$Values .= ", ";
+			}
+			$Fields .= $name;
+			$Values .= $value;
+		}
+		reset($this->FieldValues);
+		$sReturn .= "($Fields) ";
+		$sReturn .= "values ($Values)";
+		$this->WriteDebug($sReturn);
+		return $sReturn;
+	}
+	
+	// Returns a select statement
+	function GetSelect($SelectPrefix = "") {
+		$sReturn = $SelectPrefix." select ";
+		$sReturn .= $this->Fields." ";
+		
+		// Build the from statement
+		$sReturn .= "from ".$this->MainTable["TableName"]." ";
+		$TableAlias = ForceString($this->MainTable["TableAlias"], "");
+		if ($TableAlias != "") $sReturn .= $TableAlias." ";
+		
+		$sReturn .= $this->Joins." ";
+		if ($this->Wheres != "") $sReturn .= " where ".$this->Wheres." ";
+		if ($this->GroupBys != "") $sReturn .= " group by ".$this->GroupBys;
+		if ($this->OrderBys != "") $sReturn .= " order by ".$this->OrderBys;
+		$sReturn .= $this->Limit;
+		$this->WriteDebug($sReturn);
+		return $sReturn;
+	}
+
+	// returns an update statement
+	function GetUpdate() {
+		$sReturn = "update ";
+		$sReturn .= $this->MainTable["TableName"]." set ";
+		$Delimiter = "";
+		while (list($name, $value) = each($this->FieldValues)) {
+			$sReturn .= $Delimiter.$name."=".$value;
+			$Delimiter = ", ";
+		}
+		reset($this->FieldValues);
+		if ($this->Wheres != "") $sReturn .= " where ".$this->Wheres;
+		$this->WriteDebug($sReturn);
+		return $sReturn;
+	}
+	
+	// Takes the current where clause and wraps it in parentheses
+	function GroupWheres() {
+		$this->Wheres = "(".$this->Wheres.") ";
+	}
+	
+	// If the user specifies two selectfrom's, this will effectively overwrite any previous items with the current one
+	function SetMainTable($TableName, $TableAlias = "") {
+		$this->MainTable = array("TableName" => $this->TablePrefix.$TableName, "TableAlias" => $TableAlias);
+	}
+
+	function StartWhereGroup() {
+		$this->Wheres .= " (";
+	}
+	
+	function SqlBuilder(&$Context) {
+		$this->Context = &$Context;
+		$this->Clear();
+	}
+	
+	function WriteDebug($String) {
+		if ($this->Context->Session->User) {
+			if ($this->Context->Session->User->Permission("PERMISSION_ALLOW_DEBUG_INFO") && $this->Context->Mode == MODE_DEBUG) $this->Context->SqlCollector->Add($String);
+		}
+	}
+}
+?>
---
+++ library/Framework/Utility.Control.ExtensionForm.php
@@ -0,0 +1,192 @@
+<?php
+/*
+* Copyright 2003 - 2005 Mark O'Sullivan
+* This file is part of Vanilla.
+* Vanilla is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+* Vanilla is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* The latest source code for Vanilla is available at www.lussumo.com
+* Contact Mark O'Sullivan at mark [at] lussumo [dot] com
+*
+* Description: The ExtensionForm control is used to turn extensions on and off in Vanilla.
+*/
+
+class ExtensionForm extends PostBackControl {
+	
+	var $EnabledExtensions;
+   var $DisabledExtensions;
+	
+	// New DefineExtensions Method by Clay Loveless [clay at killersoft dot com] - 2005-08-05
+	function DefineExtensions() {
+		// We will want to check current Extensions to see if 
+		// they are currently used by the system. Read current Extensions
+		// once here.
+		$CurrExtensions = array();
+		$CurrentExtensions = @file($this->Context->Configuration["APPLICATION_PATH"].'conf/extensions.php');
+		if (!$CurrentExtensions) {
+			$this->Context->WarningCollector->Add($this->Context->GetDefinition('ErrReadFileExtensions').$this->Context->Configuration["APPLICATION_PATH"].'conf/extensions.php');
+		} else {
+			foreach ($CurrentExtensions as $ExLine) {
+				if (substr($ExLine, 0, 7) == 'include') {
+					$CurrExtensions[] = substr(trim($ExLine), 43, -3);
+				}
+			}
+		}
+		 
+		// Examine Extensions directory
+		$FolderHandle = @opendir($this->Context->Configuration["EXTENSIONS_PATH"]);
+		if (!$FolderHandle) {
+			$this->Context->WarningCollector->Add(
+				str_replace("//1", $this->Context->Configuration["EXTENSIONS_PATH"], $this->Context->GetDefinition('ErrOpenDirectoryExtensions')));
+		} else {
+			// Loop through each Extension file
+			while (false !== ($Item = readdir($FolderHandle))) {
+				$Extension = $this->Context->ObjectFactory->NewObject($this->Context, 'Extension');
+				$RecordItem = true;
+				// skip directories and hidden files
+				if (strlen($Item) < 1) {
+					continue;
+				}
+				if ($Item{0} == '.' || is_dir($this->Context->Configuration["EXTENSIONS_PATH"].$Item)) {
+					continue;
+				}
+				// Retrieve Extension properties
+				$Lines = @file($this->Context->Configuration["EXTENSIONS_PATH"].$Item);
+				if (!$Lines) {
+					$this->Context->WarningCollector->Add($this->Context->GetDefinition('ErrReadExtensionDefinition')." {$Item}");                
+				} else {
+					// We only examine the first 30 lines of the file
+					$Header = array_slice($Lines, 0, 30);
+					$Extension->FileName = $Item;
+					foreach ($Header as $CurrentLine) {
+						@list($key, $val) = @explode(': ', trim($CurrentLine), 2);
+						switch ($key) {
+							case 'Extension Name':
+								$Extension->Name = FormatStringForDisplay($val);
+								break;
+							case 'Extension Url':
+								$Extension->Url = FormatStringForDisplay($val);
+								break;
+							case 'Description':
+								$Extension->Description = FormatStringForDisplay($val);
+								break;
+							case 'Version':
+								$Extension->Version = FormatStringForDisplay($val);
+								break;
+							case 'Author':
+								$Extension->Author = FormatStringForDisplay($val);
+								break;
+							case 'Author Url':
+								$Extension->AuthorUrl = FormatStringForDisplay($val);
+								break;
+							default:
+								// nothing
+						}
+					}
+					if ($Extension->IsValid()) {
+						if (in_array($Item, $CurrExtensions)) {
+							$this->EnabledExtensions[$this->RemoveFileExtension($Extension->FileName)] = $Extension;
+						} else {
+							$this->DisabledExtensions[$this->RemoveFileExtension($Extension->FileName)] = $Extension;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	function RemoveFileExtension($FileName) {
+		$FileNameArray = explode(".", $FileName);
+		array_pop($FileNameArray); // Pop the extension off the filename
+		return implode(".", $FileNameArray);
+	}
+	
+	function ExtensionForm(&$Context) {
+      $this->Name = "ExtensionForm";
+		$this->ValidActions = array("Extensions", "ProcessExtension");
+		$this->Constructor($Context);
+		if (!$this->Context->Session->User->Permission("PERMISSION_MANAGE_EXTENSIONS")) {
+			$this->IsPostBack = 0;
+		} elseif ($this->IsPostBack) {
+	      $this->Extensions = array();
+			$this->DefineExtensions();
+			if ($this->PostBackAction == "ProcessExtension") {
+				$Extension = false;
+				$ExtensionKey = ForceIncomingString("ExtensionKey", "");
+				$ExtensionInUse = false;
+				// Grab that extension from the extension array
+            if (array_key_exists($ExtensionKey, $this->EnabledExtensions)) {
+					// The extension is currently enabled, so disable it
+	            $Extension = $this->EnabledExtensions[$ExtensionKey];
+					$ExtensionInUse = true;
+					$this->DelegateParameters["ExtensionName"] = $ExtensionKey;
+					$this->CallDelegate("PostExtensionDisable");
+					
+				} elseif (array_key_exists($ExtensionKey, $this->DisabledExtensions)) {
+					// The extension is currently disabled, so enable it
+	            $Extension = $this->DisabledExtensions[$ExtensionKey];
+					$this->DelegateParameters["ExtensionName"] = $ExtensionKey;
+					$this->CallDelegate("PostExtensionEnable");
+               
+				} else {
+					$this->Context->WarningCollector->Add($this->Context->GetDefinition("ErrExtensionNotFound"));
+				}
+				if ($Extension) {
+					// Open the extensions file for editing
+               $ExtensionsFile = $this->Context->Configuration["APPLICATION_PATH"]."conf/extensions.php";
+					$CurrentExtensions = @file($ExtensionsFile);
+					if (!$CurrentExtensions) {
+						$this->Context->WarningCollector->Add($this->Context->GetDefinition("ErrReadFileExtensions")." ".$this->Context->Configuration["APPLICATION_PATH"]."conf/extensions.php");
+					} else {
+						// Loop through the lines
+                  $ExtensionCount = count($CurrentExtensions);
+						for ($j = 0; $j < $ExtensionCount; $j++) {
+							if ($ExtensionInUse) {
+								if (trim($CurrentExtensions[$j]) == "include(\$Configuration[\"EXTENSIONS_PATH\"].\"".$Extension->FileName."\");") {
+									// If the extension is currently in use, remove it
+									array_splice($CurrentExtensions, $j, 1);
+									$j = count($CurrentExtensions);
+								}
+							} elseif (trim($CurrentExtensions[$j]) == "?>") {
+								// If the extension is NOT currently in use, add it
+								$CurrentExtensions[$j] = "include(\$Configuration[\"EXTENSIONS_PATH\"].\"".$Extension->FileName."\");\r\n";
+								$CurrentExtensions[] = "?>";
+								$j = count($CurrentExtensions);
+							}
+						}
+						// Save the extensions file
+						// Open for writing only.
+						// Place the file pointer at the beginning of the file and truncate the file to zero length. 
+						$FileHandle = @fopen($ExtensionsFile, "wb");
+						$FileContents = implode("", $CurrentExtensions);
+						if (!$FileHandle) {
+							$this->Context->WarningCollector->Add(str_replace("//1", $ExtensionsFile, $this->Context->GetDefinition("ErrOpenFile")));
+						} else {
+							if (!@fwrite($FileHandle, $FileContents)) $this->Context->WarningCollector->Add($this->Context->GetDefinition("ErrWriteFile"));
+						}
+						@fclose($FileHandle);
+						
+						// If everything was successful, redirect back to this page
+						if ($this->Context->WarningCollector->Iif()) {
+							header("Location:".GetUrl($this->Context->Configuration, $this->Context->SelfUrl, "", "", "", "", "PostBackAction=Extensions&Detail=".$ExtensionKey."#".$ExtensionKey));
+							die();
+						} else {
+							$this->PostBackAction = "Extensions";
+						}
+					}
+				}
+			}
+		}
+      $this->CallDelegate("Constructor");
+	}
+	
+	function Render() {
+		if ($this->IsPostBack) {
+			$SelectedExtensionKey = ForceIncomingString("Detail", "");
+			$this->CallDelegate("PreRender");
+			include($this->Context->Configuration["THEME_PATH"]."templates/settings_extensions.php");
+			$this->CallDelegate("PostRender");
+		}
+	}
+}
+?>
