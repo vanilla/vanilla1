827296579347d0cad0fa95975bfe19d01b90d102
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2005-12-23 19:06:10 +0000

    Removed Debug Code

--- themes/vanilla/templates/foot.php
+++ themes/vanilla/templates/foot.php
@@ -9,10 +9,10 @@ $AllowDebugInfo = 0;
 if ($this->Context->Session->User) {
    if ($this->Context->Session->User->Permission("PERMISSION_ALLOW_DEBUG_INFO")) $AllowDebugInfo = 1;
 }
-// if ($this->Context->Mode == MODE_DEBUG && $AllowDebugInfo) {
+if ($this->Context->Mode == MODE_DEBUG && $AllowDebugInfo) {
    echo("<div class=\"DebugBar\" id=\"DebugBar\">
    <b>Debug Options</b> | Resize: <a href=\"javascript:window.resizeTo(800,600);\">800x600</a>, <a href=\"javascript:window.resizeTo(1024, 768);\">1024x768</a> | <a href=\"javascript:HideElement('DebugBar');\">Hide This</a>");
    echo($this->Context->SqlCollector->GetMessages());
    echo("</div>");
-// }
+}
 ?>
