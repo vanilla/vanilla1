1c17424a86cfe9a73de141e8c95b688ccc90e177
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-01-04 15:58:07 +0000

    Changed sql files to unix format

--- setup/sqlite.sql
+++ setup/sqlite.sql
@@ -1,15 +1,3 @@
-/*
-* Copyright 2003 - 2005 Mark O'Sullivan
-* This file is part of Vanilla.
-* Vanilla is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-* Vanilla is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-* The latest source code for Vanilla is available at www.lussumo.com
-* Contact Mark O'Sullivan at mark [at] lussumo [dot] com
-*
-* Description: Shell SQLite database - for use with a fresh, manual install of Vanilla 1.0
-*/
-
 CREATE TABLE LUM_User (
   UserID integer primary key,
   RoleID integer NOT NULL default '0',
