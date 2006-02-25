45e4eede27d34586f810abef79b27d1caf2f8a37
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-02-25 15:39:21 +0000

    Shifting around theme structure

--- trunk/themes/vanilla/templates/account_password_form.php
+++ trunk/themes/vanilla/templates/account_password_form.php
@@ -1,40 +1,50 @@
 <?php
 // Note: This file is included from the library/Vanilla.Control.PasswordForm.php class.
 
-echo('<div class="AccountForm">
-   <h1>'.$this->Context->GetDefinition('ChangeYourPassword').'</h1>
-   <div class="Form AccountPassword">');
-      
-      $this->CallDelegate('PreWarningsRender');
-      
-      echo($this->Get_Warnings()
-      .$this->Get_PostBackForm('frmAccountPassword'));
-      
-      $this->CallDelegate('PreInputsRender');
-      
-      echo('<dl>
-         <dt>'.$this->Context->GetDefinition('YourOldPassword').'</dt>
-         <dd><input type="password" name="OldPassword" value="'.$this->User->OldPassword.'" maxlength="100" class="SmallInput" id="txtOldPassword" /> '.$Required.'</dd>
-      </dl>
-      <div class="InputNote">'.$this->Context->GetDefinition('YourOldPasswordNotes').'</div>
-      <dl>
-         <dt>'.$this->Context->GetDefinition('YourNewPassword').'</dt>
-         <dd><input type="password" name="NewPassword" value="'.$this->User->NewPassword.'" maxlength="100" class="SmallInput" id="txtNewPassword" /> '.$Required.'</dd>
-      </dl>
-      <div class="InputNote">'.$this->Context->GetDefinition('YourNewPasswordNotes').'</div>
-      <dl>
-         <dt>'.$this->Context->GetDefinition('YourNewPasswordAgain').'</dt>
-         <dd><input type="password" name="ConfirmPassword" value="'.$this->User->ConfirmPassword.'" maxlength="100" class="SmallInput" id="txtConfirmPassword" /> '.$Required.'</dd>
-      </dl>
-      <div class="InputNote">'.$this->Context->GetDefinition('YourNewPasswordAgainNotes').'</div>');
-      
-      $this->CallDelegate('PreButtonsRender');
-      
-      echo('<div class="FormButtons">
-         <input type="submit" name="btnSave" value="'.$this->Context->GetDefinition('Save').'" class="Button SubmitButton" />
-         <a href="'.GetUrl($this->Context->Configuration, "account.php", "", "u", $this->User->UserID).'" class="CancelButton">'.$this->Context->GetDefinition('Cancel').'</a>
+if ($this->Context->Session->UserID != $this->User->UserID && !$this->Context->Session->User->Permission('PERMISSION_EDIT_USERS')) {
+   $this->Context->WarningCollector->Add($this->Context->GetDefinition('PermissionError'));
+   echo('<div class="AccountForm">
+      '.$this->Get_Warnings().'
+   </div>');				
+} else {				
+   $this->PostBackParams->Set('PostBackAction', 'ProcessPassword');
+   $this->PostBackParams->Set('u', $this->User->UserID);
+   $Required = $this->Context->GetDefinition('Required');
+   echo('<div class="AccountForm">
+      <h1>'.$this->Context->GetDefinition('ChangeYourPassword').'</h1>
+      <div class="Form AccountPassword">');
+         
+         $this->CallDelegate('PreWarningsRender');
+         
+         echo($this->Get_Warnings()
+         .$this->Get_PostBackForm('frmAccountPassword'));
+         
+         $this->CallDelegate('PreInputsRender');
+         
+         echo('<dl>
+            <dt>'.$this->Context->GetDefinition('YourOldPassword').'</dt>
+            <dd><input type="password" name="OldPassword" value="'.$this->User->OldPassword.'" maxlength="100" class="SmallInput" id="txtOldPassword" /> '.$Required.'</dd>
+         </dl>
+         <div class="InputNote">'.$this->Context->GetDefinition('YourOldPasswordNotes').'</div>
+         <dl>
+            <dt>'.$this->Context->GetDefinition('YourNewPassword').'</dt>
+            <dd><input type="password" name="NewPassword" value="'.$this->User->NewPassword.'" maxlength="100" class="SmallInput" id="txtNewPassword" /> '.$Required.'</dd>
+         </dl>
+         <div class="InputNote">'.$this->Context->GetDefinition('YourNewPasswordNotes').'</div>
+         <dl>
+            <dt>'.$this->Context->GetDefinition('YourNewPasswordAgain').'</dt>
+            <dd><input type="password" name="ConfirmPassword" value="'.$this->User->ConfirmPassword.'" maxlength="100" class="SmallInput" id="txtConfirmPassword" /> '.$Required.'</dd>
+         </dl>
+         <div class="InputNote">'.$this->Context->GetDefinition('YourNewPasswordAgainNotes').'</div>');
+         
+         $this->CallDelegate('PreButtonsRender');
+         
+         echo('<div class="FormButtons">
+            <input type="submit" name="btnSave" value="'.$this->Context->GetDefinition('Save').'" class="Button SubmitButton" />
+            <a href="'.GetUrl($this->Context->Configuration, "account.php", "", "u", $this->User->UserID).'" class="CancelButton">'.$this->Context->GetDefinition('Cancel').'</a>
+         </div>
+         </form>
       </div>
-      </form>
-   </div>
-</div>');
+   </div>');
+}
 ?>
