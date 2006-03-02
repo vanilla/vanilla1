e492def63ec003e4c84e0d7a62ca761a92fd5cd7
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-02 21:56:03 +0000

    Changing xhtml

--- themes/vanilla1/settings_extensions.php
+++ themes/vanilla1/settings_extensions.php
@@ -1,27 +1,27 @@
 <?php
 // Note: This file is included from the library/Vanilla.Control.ExtensionForm.php control.
 
-echo('<div class="SettingsForm">
-   <h1>'.$this->Context->GetDefinition('Extensions').'</h1>
-   <div class="ExtensionsForm">'
+echo('<div id="Form" class="Account Extensions">
+   <fieldset>
+      <legend>'.$this->Context->GetDefinition('Extensions').'</legend>'
       .$this->Get_Warnings()
-      .'<div class="InputNote">
-         '.$this->Context->GetDefinition('ExtensionFormNotes').'
-      </div>
-      <table border="0" cellpadding="0" cellspacing="0" summary="">
-         <tr valign="top">
-            <td class="DisabledExtensions">
-               <h2>'.$this->Context->GetDefinition('DisabledExtensions').'</h2>');
+      .'<form action="#" method="post">
+		<p>'.$this->Context->GetDefinition('ExtensionFormNotes').'</p>
+      
+      <div class="ExtensionListContainer clearfix">
+         <div class="ExtensionsDisabled">
+            <h2>'.$this->Context->GetDefinition('DisabledExtensions').'</h2>
+            <ul>');
                if (is_array($this->DisabledExtensions)) {
-                  $ExtensionList = '<dl class="Extensions">';
+                  $ExtensionList = '';
                   ksort($this->DisabledExtensions);
                   $FirstExtension = 1;
                   while (list($ExtensionKey, $Extension) = each($this->DisabledExtensions)) {
-                     $ExtensionList .= '<dt class="DisabledExtension'.($ExtensionKey == $SelectedExtensionKey ? ' SelectedExtension' : '').($FirstExtension?' FirstExtension':'').'">'
-                        .'<a name="'.$ExtensionKey.'"></a>'
-                        .GetDynamicCheckBox('chk'.$ExtensionKey, 1, 0, "document.location='".GetUrl($this->Context->Configuration, $this->Context->SelfUrl, "", "", "", "", "PostBackAction=ProcessExtension&amp;ExtensionKey=".$ExtensionKey)."';", '<strong>'.$Extension->Name.'</strong> '.$Extension->Version)
-                        .'</dt>
-                        <dd class="DisabledExtension'.($ExtensionKey == $SelectedExtensionKey ? ' SelectedExtension' : '').($FirstExtension?' FirstExtension':'').'">';
+                     $ExtensionList .= '<li>
+                        <h3>
+                           '.GetDynamicCheckBox('chk'.$ExtensionKey, 1, 0, "document.location='".GetUrl($this->Context->Configuration, $this->Context->SelfUrl, "", "", "", "", "PostBackAction=ProcessExtension&amp;ExtensionKey=".$ExtensionKey)."';", $Extension->Name.' <span>'.$Extension->Version.'</span>').'
+                        </h3>
+                        <p>';
                         if ($ExtensionKey == $SelectedExtensionKey) {
                            $ExtensionList .= $Extension->Description
                            .'<br />'.FormatHyperlink($Extension->Url)
@@ -29,26 +29,28 @@ echo('<div class="SettingsForm">
                         } else {
                            $ExtensionList .= '<a href="'.GetUrl($this->Context->Configuration, $this->Context->SelfUrl, '', '', '', '', 'PostBackAction=Extensions&amp;Detail='.$ExtensionKey.'#'.$ExtensionKey).'">'.SliceString($Extension->Description, 60).'</a>';
                         }
-                        $ExtensionList .= '</dd>';
-                     $FirstExtension = 0;
+                        $ExtensionList .= '<p>
+                     </li>';
                   }
-                  echo($ExtensionList . '</dl>');
+                  echo($ExtensionList);
                } else {
-                  echo($this->Context->GetDefinition('NoDisabledExtensions'));
+                  echo('<li><p>'.$this->Context->GetDefinition('NoDisabledExtensions').'</p></li>');
                }
-            echo('</td>
-            <td class="EnabledExtensions">
-               <h2>'.$this->Context->GetDefinition('EnabledExtensions').'</h2>');
+            echo('</ul>
+         </div>
+         <div class="ExtensionsEnabled">
+            <h2>
+               '.$this->Context->GetDefinition('EnabledExtensions').'
+            </h2>
+            <ul>');
                if (is_array($this->EnabledExtensions)) {
-                  $ExtensionList = '<dl class="Extensions">';
+                  $ExtensionList = '';
                   ksort($this->EnabledExtensions);
                   $FirstExtension = 1;
                   while (list($ExtensionKey, $Extension) = each($this->EnabledExtensions)) {
-                     $ExtensionList .= '<dt class="EnabledExtension'.($ExtensionKey == $SelectedExtensionKey ? ' SelectedExtension' : '').($FirstExtension?' FirstExtension':'').'">'
-                        .'<a name="'.$ExtensionKey.'"></a>'
-                        .GetDynamicCheckBox('chk'.$ExtensionKey, 1, 1, "document.location='".GetUrl($this->Context->Configuration, $this->Context->SelfUrl, "", "", "", "", "PostBackAction=ProcessExtension&amp;ExtensionKey=".$ExtensionKey)."';", '<strong>'.$Extension->Name.'</strong> '.$Extension->Version)
-                        .'</dt>
-                        <dd class="EnabledExtension'.($ExtensionKey == $SelectedExtensionKey ? ' SelectedExtension' : '').($FirstExtension?' FirstExtension':'').'">';
+                     $ExtensionList .= '<li>
+                        <h3>'.GetDynamicCheckBox('chk'.$ExtensionKey, 1, 1, "document.location='".GetUrl($this->Context->Configuration, $this->Context->SelfUrl, "", "", "", "", "PostBackAction=ProcessExtension&amp;ExtensionKey=".$ExtensionKey)."';", $Extension->Name.' <span>'.$Extension->Version.'</span>').'</h3>
+                        <p>';
                         if ($ExtensionKey == $SelectedExtensionKey) {
                            $ExtensionList .= $Extension->Description
                            .'<br />'.FormatHyperlink($Extension->Url)
@@ -56,16 +58,17 @@ echo('<div class="SettingsForm">
                         } else {
                            $ExtensionList .= '<a href="'.GetUrl($this->Context->Configuration, $this->Context->SelfUrl, '', '', '', '', 'PostBackAction=Extensions&amp;Detail='.$ExtensionKey.'#'.$ExtensionKey).'">'.SliceString($Extension->Description, 60).'</a>';
                         }
-                        $ExtensionList .= '</dd>';
-                     $FirstExtension = 0;
+                        $ExtensionList .= '</p>
+                     </li>';
                   }
-                  echo($ExtensionList.'</dl>');
+                  echo($ExtensionList);
                } else {
-                  echo($this->Context->GetDefinition('NoEnabledExtensions'));
+                  echo('<li><p>'.$this->Context->GetDefinition('NoEnabledExtensions').'</p></li>');
                }
-            echo('</td>
-         </tr>
-      </table>					
-   </div>
+            echo('</ul>
+         </div>
+      </div>
+      </form>					
+   </fieldset>
 </div>');
 ?>
