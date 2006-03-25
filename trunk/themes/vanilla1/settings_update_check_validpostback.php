0bd39712df69b48865c9c1d77ca3616ab425f7bf
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-25 18:25:06 +0000

    xhtml changes

--- trunk/themes/vanilla1/settings_update_check_validpostback.php
+++ trunk/themes/vanilla1/settings_update_check_validpostback.php
@@ -1,10 +1,12 @@
 <?php
-// Note: This file is included from the library/Vanilla.Control.UpdateCheck.php control.
+// Note: This file is included from the library/Framework/Framework.Control.UpdateCheck.php control.
 
-echo('<div class="SettingsForm">
-   <h1>'.$this->Context->GetDefinition('UpdateCheck').'</h1>
-   <div class="Form UpdateForm">
-      <div class="InputNote">'.$Message.'</div>
-   </div>
-</div>');
+echo '<div id="Form" class="Account UpdateCheck">
+   <fieldset>
+      <legend>'.$this->Context->GetDefinition('UpdateCheck').'</legend>
+      <form id="frmUpdateCheck" method="post" action="">
+      <p class="Description">'.$this->LussumoMessage.'</p>
+      </form>
+   </fieldset>
+</div>';
 ?>
