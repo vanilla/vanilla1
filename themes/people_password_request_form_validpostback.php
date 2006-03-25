74c3f5d3f5c3768d3473547073149ae77ca84b42
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-25 20:05:52 +0000

    Adding new templates

---
+++ themes/people_password_request_form_validpostback.php
@@ -0,0 +1,12 @@
+<?php
+// Note: This file is included from the library/People.Control.PasswordRequestForm.php control.
+
+echo('<div class="FormComplete">
+   <h1>'.$this->Context->GetDefinition('RequestProcessed').'</h1>
+   <ul>
+      <li>'.str_replace('//1',
+         FormatStringForDisplay($this->EmailSentTo, 1),
+         $this->Context->GetDefinition('MessageSentToXContainingPasswordInstructions')).'</li>
+   </ul>
+</div>');
+?>
