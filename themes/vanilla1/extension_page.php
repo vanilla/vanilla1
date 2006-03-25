3fbacaa1749d7be8c49153b36272a0a57a83cd0a
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-25 20:06:36 +0000

    Removing temporary templates

--- themes/vanilla1/extension_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($this->PostBackAction == '') {
-   // Note: This file is included from the library/Vanilla.Control.DiscussionGrid.php class.
-   echo('<div class="Title">'.$this->Context->GetDefinition('AboutExtensionPage').'</div>
-   <div class="SettingsBody">
-     '.$this->Context->GetDefinition('AboutExtensionPageNotes').'
-   </div>');
-}
-?>
--- themes/vanilla1/people_banner.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Note: This file is included from the library/Utility.Control.Filler.php class for "People" pages.
-
-echo('<div class="SiteContainer '.$this->Properties['CssClass'].'">
-   <div class="PageTitle">'.$this->Context->Configuration['BANNER_TITLE'].'</div>
-   <div class="FormContainer">');
-   
-?>
