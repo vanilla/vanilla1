150d4402d87697997ba889819a084f74c7f9cd32
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-25 20:05:08 +0000

    Removing old templates

--- themes/page_end.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Note: This file is included from the library/Utility.Control.PageEnd.php class.
-
-
-echo('</body>
-</html>');
-?>
--- themes/search_results_bottom.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Note: This file is included from the library/Vanilla.Control.SearchForm.php class.
-
-echo($this->PageList
-	.$this->PageDetails
-	.'<a class="PageJump Top" href="'.GetRequestUri().'#pgtop">'.$this->Context->GetDefinition('TopOfPage').'</a>');
-   
-?>
