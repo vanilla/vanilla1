12ed2c03b989a94414f7280afd51307bd7424a68
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-03-27 19:27:09 +0000

    Removing IE Quirks mode

--- trunk/themes/head.php
+++ trunk/themes/head.php
@@ -1,8 +1,7 @@
 <?php
 // Note: This file is included from the library/Framework/Framework.Control.Head.php class.
 
-$HeadString = '<'.chr(63).'xml version="1.0" encoding="utf-8"'.chr(63).'>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+$HeadString = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
    <head>
       <title>'.$this->Context->Configuration['APPLICATION_TITLE'].' - '.$this->Context->PageTitle.'</title>
