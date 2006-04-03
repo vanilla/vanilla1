45adb36764704108a5d342d0f370b9f41e782476
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-04-03 16:49:45 +0000

    Fixed bug in comment protection extension that allowed bad js to be executed

--- trunk/extensions/ExtendedApplicationForm/default.php
+++ trunk/extensions/ExtendedApplicationForm/default.php
@@ -26,46 +26,46 @@ $Context->Dictionary["LastNameLower"] = "last name";
 $Context->Dictionary["ErrReadTOS"] = "You must READ the terms of service.";
 
 
-if ($Context->SelfUrl == "people.php" && in_array(ForceIncomingString("PostBackAction", ""), array("ApplyForm", "Apply"))) {
+if ($Context->SelfUrl == 'people.php' && in_array(ForceIncomingString('PostBackAction', ''), array('ApplyForm', 'Apply'))) {
    
    // Add the Real name inputs to the application form
    function ApplicationForm_AddRealNameInputs($ApplyForm) {
-      echo("<dt>".$ApplyForm->Context->GetDefinition("FirstName")."</dt>
-      <dd><input type=\"text\" name=\"FirstName\" value=\"".$ApplyForm->Applicant->FirstName."\" class=\"Input\" maxlength=\"40\" /></dd>
-      <dt>".$ApplyForm->Context->GetDefinition("LastName")."</dt>
-      <dd><input type=\"text\" name=\"LastName\" value=\"".$ApplyForm->Applicant->LastName."\" class=\"Input\" maxlength=\"40\" /></dd>");
+      echo '<dt>'.$ApplyForm->Context->GetDefinition('FirstName').'</dt>
+      <dd><input type="text" name="FirstName" value="'.$ApplyForm->Applicant->FirstName.'" class="Input" maxlength="40" /></dd>
+      <dt>'.$ApplyForm->Context->GetDefinition('LastName').'</dt>
+      <dd><input type="text" name="LastName" value="'.$ApplyForm->Applicant->LastName.'" class="Input" maxlength="40" /></dd>';
    }
    
-   $Context->AddToDelegate("ApplyForm",
-      "PreInputsRender",
-      "ApplicationForm_AddRealNameInputs");
+   $Context->AddToDelegate('ApplyForm',
+      'PreInputsRender',
+      'ApplicationForm_AddRealNameInputs');
       
       
    // Add the requirements to the membership application processing
    function ApplicationForm_AddRequirements(&$ApplyForm) {
       $SafeUser = $ApplyForm->Applicant;
       $SafeUser->FormatPropertiesForDatabaseInput();
-		Validate($ApplyForm->Context->GetDefinition("FirstNameLower"), 1, $SafeUser->FirstName, 50, "", $ApplyForm->Context);
-		Validate($ApplyForm->Context->GetDefinition("LastNameLower"), 1, $SafeUser->LastName, 50, "", $ApplyForm->Context);
+		Validate($ApplyForm->Context->GetDefinition('FirstNameLower'), 1, $SafeUser->FirstName, 50, "", $ApplyForm->Context);
+		Validate($ApplyForm->Context->GetDefinition('LastNameLower'), 1, $SafeUser->LastName, 50, "", $ApplyForm->Context);
       // Make sure that they actually read the terms of service
 		if (!$SafeUser->ReadTerms) $ApplyForm->Context->WarningCollector->Add($ApplyForm->Context->GetDefinition("ErrReadTOS"));
    }
 
-   $Context->AddToDelegate("ApplyForm",
-      "PreCreateUser",
-      "ApplicationForm_AddRequirements");
+   $Context->AddToDelegate('ApplyForm',
+      'PreCreateUser',
+      'ApplicationForm_AddRequirements');
       
    // Add the requirement to the identity form on the account page as well
    function IdentityForm_AddRequirements(&$IdentityForm) {
       $SafeUser = $IdentityForm->User;
       $SafeUser->FormatPropertiesForDatabaseInput();
-		Validate($IdentityForm->Context->GetDefinition("FirstNameLower"), 1, $SafeUser->FirstName, 50, "", $IdentityForm->Context);
-		Validate($IdentityForm->Context->GetDefinition("LastNameLower"), 1, $SafeUser->LastName, 50, "", $IdentityForm->Context);
+		Validate($IdentityForm->Context->GetDefinition('FirstNameLower'), 1, $SafeUser->FirstName, 50, '', $IdentityForm->Context);
+		Validate($IdentityForm->Context->GetDefinition('LastNameLower'), 1, $SafeUser->LastName, 50, '', $IdentityForm->Context);
    }
 
-   $Context->AddToDelegate("IdentityForm",
-      "PreSaveIdentity",
-      "IdentityForm_AddRequirements");   
+   $Context->AddToDelegate('IdentityForm',
+      'PreSaveIdentity',
+      'IdentityForm_AddRequirements');   
    
 }
 ?>
--- trunk/extensions/QuickKeys/default.php
+++ trunk/extensions/QuickKeys/default.php
@@ -19,74 +19,64 @@ You should cut & paste these language definitions into your
 conf/your_language.php file (replace "your_language" with your chosen language,
 of course):
 */
-$Context->Dictionary["MenuOptions"] = "Menu Options";
-$Context->Dictionary["UseQuickKeys"] = "Use quick-keys to access common forum pages";
-$Context->Dictionary["Discussions_QuickKey"] = "<em>D</em>iscussions";
-$Context->Dictionary["Categories_QuickKey"] = "<em>C</em>ategories";
-$Context->Dictionary["Search_QuickKey"] = "<em>S</em>earch";
-$Context->Dictionary["Settings_QuickKey"] = "S<em>e</em>ttings";
-$Context->Dictionary["Account_QuickKey"] = "<em>A</em>ccount";
-$Context->Dictionary["StartANewDiscussion_Quickkey"] = "Start a <em>n</em>ew discussion";
+$Context->Dictionary['MenuOptions'] = 'Menu Options';
+$Context->Dictionary['UseQuickKeys'] = 'Use quick-keys to access common forum pages';
+$Context->Dictionary['Discussions_QuickKey'] = '<em>D</em>iscussions';
+$Context->Dictionary['Categories_QuickKey'] = '<em>C</em>ategories';
+$Context->Dictionary['Search_QuickKey'] = '<em>S</em>earch';
+$Context->Dictionary['Settings_QuickKey'] = 'S<em>e</em>ttings';
+$Context->Dictionary['Account_QuickKey'] = '<em>A</em>ccount';
 
 
-if (in_array($Context->SelfUrl, array("account.php", "categories.php", "comments.php", "index.php", "post.php", "search.php", "settings.php")) && $Context->Session->UserID > 0) {
+if (in_array($Context->SelfUrl, array('account.php', 'categories.php', 'comments.php', 'index.php', 'post.php', 'search.php', 'settings.php')) && $Context->Session->UserID > 0) {
    if (@$Menu && @$Context->Session->User) {
-      if ($Context->Session->User->Preference("UseQuickKeys")) {
+      if ($Context->Session->User->Preference('UseQuickKeys')) {
          // Clear out existing tabs and put in the new quickkey tabs
          $Menu->ClearTabs();
-         $Menu->AddTab($Context->GetDefinition("Discussions_QuickKey"), "discussions", GetUrl($Configuration, "index.php"), "accesskey=\"d\"");
-         if ($Context->Configuration["USE_CATEGORIES"]) $Menu->AddTab($Context->GetDefinition("Categories_QuickKey"), 'categories', GetUrl($Configuration, "categories.php"), "accesskey=\"c\"");
-         $Menu->AddTab($Context->GetDefinition("Search_QuickKey"), "search", GetUrl($Configuration, "search.php"), "accesskey=\"s\"");
+         $Menu->AddTab($Context->GetDefinition('Discussions_QuickKey'), 'discussions', GetUrl($Configuration, 'index.php'), 'accesskey=\'d\'');
+         if ($Context->Configuration['USE_CATEGORIES']) $Menu->AddTab($Context->GetDefinition('Categories_QuickKey'), 'categories', GetUrl($Configuration, 'categories.php'), 'accesskey=\'c\'');
+         $Menu->AddTab($Context->GetDefinition('Search_QuickKey'), 'search', GetUrl($Configuration, 'search.php'), 'accesskey=\'s\'');
 
 			// Make sure they should be seeing the settings tab
-			$RequiredPermissions = array("PERMISSION_CHECK_FOR_UPDATES",
-				"PERMISSION_APPROVE_APPLICANTS",
-				"PERMISSION_MANAGE_REGISTRATION",
-				"PERMISSION_ADD_ROLES",
-				"PERMISSION_EDIT_ROLES",
-				"PERMISSION_REMOVE_ROLES",
-				"PERMISSION_ADD_CATEGORIES",
-				"PERMISSION_EDIT_CATEGORIES",
-				"PERMISSION_REMOVE_CATEGORIES",
-				"PERMISSION_SORT_CATEGORIES",
-				"PERMISSION_CHANGE_APPLICATION_SETTINGS",
-				"PERMISSION_MANAGE_EXTENSIONS",
-				"PERMISSION_MANAGE_LANGUAGE",
-				"PERMISSION_MANAGE_STYLES");
+			$RequiredPermissions = array('PERMISSION_CHECK_FOR_UPDATES',
+				'PERMISSION_APPROVE_APPLICANTS',
+				'PERMISSION_MANAGE_REGISTRATION',
+				'PERMISSION_ADD_ROLES',
+				'PERMISSION_EDIT_ROLES',
+				'PERMISSION_REMOVE_ROLES',
+				'PERMISSION_ADD_CATEGORIES',
+				'PERMISSION_EDIT_CATEGORIES',
+				'PERMISSION_REMOVE_CATEGORIES',
+				'PERMISSION_SORT_CATEGORIES',
+				'PERMISSION_CHANGE_APPLICATION_SETTINGS',
+				'PERMISSION_MANAGE_EXTENSIONS',
+				'PERMISSION_MANAGE_LANGUAGE',
+				'PERMISSION_MANAGE_STYLES');
 				
 			$RequiredPermissionsCount = count($RequiredPermissions);
 			for ($i = 0; $i < $RequiredPermissionsCount; $i++) {
 				if ($Context->Session->User->Permission($RequiredPermissions[$i])) {
-					$Menu->AddTab($Context->GetDefinition("Settings_QuickKey"), "settings", GetUrl($Configuration, "settings.php"), "accesskey=\"e\"");
+					$Menu->AddTab($Context->GetDefinition('Settings_QuickKey'), 'settings', GetUrl($Configuration, 'settings.php'), 'accesskey=\'e\'');
 					break;
 				}
 			}
 
-         $Menu->AddTab($Context->GetDefinition("Account_QuickKey"), "account", GetUrl($Configuration, "account.php"), "accesskey=\"a\"");
-         
-         // Set up the "Start a new discussion" button
-			$CategoryID = ForceIncomingInt("CategoryID", 0);
-			if ($CategoryID == 0) $CategoryID = "";
-         $StartANewDiscussionString = "<a class=\"PanelButton StartDiscussionButton\" href=\"".GetUrl($Configuration, "post.php", "category/", "CategoryID", $CategoryID)."\">".$Context->GetDefinition("StartANewDiscussion")."</a>";
-			$StartButtonKey = array_search($StartANewDiscussionString, $Panel->Strings);
-			if ($StartButtonKey !== false) {
-				$Panel->Strings[$StartButtonKey] = "<a class=\"PanelButton StartDiscussionButton\" href=\"".GetUrl($Configuration, "post.php", "category/", "CategoryID", $CategoryID)."\" accesskey=\"n\">".$Context->GetDefinition("StartANewDiscussion_Quickkey")."</a>";
-			}
+         $Menu->AddTab($Context->GetDefinition('Account_QuickKey'), 'account', GetUrl($Configuration, 'account.php'), 'accesskey=\'a\'');
       }
    }
 }
 
 // Add the QuickKeys setting to the forum preferences form
-if ($Context->SelfUrl == "account.php" && $Context->Session->UserID > 0) {
-	$PostBackAction = ForceIncomingString("PostBackAction", "");
-	if ($PostBackAction == "Functionality") {
+if ($Context->SelfUrl == 'account.php' && $Context->Session->UserID > 0) {
+	$PostBackAction = ForceIncomingString('PostBackAction', '');
+	if ($PostBackAction == 'Functionality') {
 		function PreferencesForm_AddQuickKeysPreference(&$PreferencesForm) {
-			$PreferencesForm->AddPreference("MenuOptions", "UseQuickKeys", "UseQuickKeys", 1);
+			$PreferencesForm->AddPreference('MenuOptions', 'UseQuickKeys', 'UseQuickKeys', 1);
 		}
 		
-		$Context->AddToDelegate("PreferencesForm",
-			"Constructor",
-			"PreferencesForm_AddQuickKeysPreference");
+		$Context->AddToDelegate('PreferencesForm',
+			'Constructor',
+			'PreferencesForm_AddQuickKeysPreference');
 	}
 }
 
