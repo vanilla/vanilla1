8d6aede37caa345b9bdc2a6895495c4e64a8ffda
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2006-08-16 22:54:53 +0000

    Changed all copyright notices to reflect 2006

--- themes/people_foot.php
+++ themes/people_foot.php
@@ -2,5 +2,5 @@
 // Note: This file is included from the library/People/People.Control.PeopleFoot.php class.
 
 echo '</div>
-<div class="Foot'.$this->CssClass.'"><a href="http://lussumo.com">Lussumo Vanilla, Swell, and People</a> Copyright &copy; 2001-2005</div>';
+<div class="Foot'.$this->CssClass.'"><a href="http://lussumo.com">Lussumo Vanilla, Swell, and People</a> Copyright &copy; 2001-2006</div>';
 ?>
