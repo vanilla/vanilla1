fd7aaf8b950a52435ef83138239f6798f03c293b
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2007-03-02 00:01:21 +0000

    Fixing the installer/upgrader & readmes.

--- trunk/appg/settings.php
+++ trunk/appg/settings.php
@@ -203,8 +203,8 @@ $Configuration['ADDON_NOTICE'] = '1';
 
 // Application versions
 define('APPLICATION', 'Vanilla');
-define('FRAMEWORK_VERSION', '1.0.1');
-define('PEOPLE_VERSION', '1.0.1');
+define('FRAMEWORK_VERSION', '1.1');
+define('PEOPLE_VERSION', '1.1');
 define('APPLICATION_VERSION', '1.1');
 
 // Application Mode Constants
