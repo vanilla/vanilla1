36ab2e8bda1fcb8718ed06b03e0aa26c0ac3d25d
mark <mark@1c12818e-1a40-11de-a108-cd2f117ce590>
2007-03-12 02:43:32 +0000

    Updated version numbers in installer/upgrader.

--- trunk/setup/installer.php
+++ trunk/setup/installer.php
@@ -418,12 +418,12 @@ if (!defined('IN_VANILLA')) exit();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
    <head>
-      <title>Vanilla 1.1.1 Installer</title>
+      <title>Vanilla 1.1.2 Installer</title>
 		<link rel="stylesheet" type="text/css" href="./style.css" />
    </head>
    <body>
       <h1>
-         <span><strong>Vanilla 1.1.1</strong> Installer</span>
+         <span><strong>Vanilla 1.1.2</strong> Installer</span>
       </h1>
 		<div class="Container">
 			<div class="Content">
