bca2645b97a26d15bf9330d2b96639b96061ff28
dinoboff <dinoboff@1c12818e-1a40-11de-a108-cd2f117ce590>
2007-08-25 13:06:24 +0000

    Added compatibility test to WriteEmail() Vanilla issue #3 http://lussumo.com/bugs/discussion/9/documentwrite-is-invalid-with-xhtml-doctypes/#Item_1

--- js/global.js
+++ js/global.js
@@ -259,15 +259,17 @@ function Wait(Sender, WaitText) {
 }
 
 function WriteEmail(i, d, n, v) {
-	var a, p, t;
-	//Create link
-	a = document.createElement('a');
-	a.href = 'mai'+'lto:'+n+'@'+'d';
-	//Create text link
-	v = v || n+"@"+d;
-	t = document.createTextNode(v);
-	a.appendChild(t);
-	//Append link
-	p = document.getElementById(i);
-	p.appendChild(a);
+	if (document.createElement && document.getElementById) {
+		var a, p, t;
+		//Create link
+		a = document.createElement('a');
+		a.href = 'mai'+'lto:'+n+'@'+'d';
+		//Create text link
+		v = v || n+"@"+d;
+		t = document.createTextNode(v);
+		a.appendChild(t);
+		//Append link
+		p = document.getElementById(i);
+		p.appendChild(a);
+	}
 }
