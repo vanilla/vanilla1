fe36fb59a02c996238369b28d931fc93e4dbb60e
dinoboff <dinoboff@1c12818e-1a40-11de-a108-cd2f117ce590>
2007-10-20 20:56:45 +0000

    see http://lussumo.com/community/discussion/7214/vanilla-113-new-remote-blind-sql-injection-exploit-/

--- src/ajax/sortroles.php
+++ src/ajax/sortroles.php
@@ -14,16 +14,19 @@
 include('../appg/settings.php');
 include('../appg/init_ajax.php');
 
-if (!$Context->Session->User->Permission('PERMISSION_EDIT_ROLES')) {
-	die();
+if (!$Context->Session->User->Permission('PERMISSION_SORT_ROLES')) {
+	die('Error permission');
 }
 
 $Sql = 'update '.$Configuration["DATABASE_TABLE_PREFIX"]."Role set Priority = '//1' where RoleID = '//2';";
 $SortOrder = ForceIncomingArray('RoleID', array());
 $ItemCount = count($SortOrder);
 for ($i = 0; $i < $ItemCount; $i++) {
-	$ExecSql = str_replace(array('//1', '//2'), array($i, ForceInt($SortOrder[$i])), $Sql);
-	$Context->Database->Execute($ExecSql, 'AJAX', 'ReorderRoles', 'Failed to reorder roles', 0);
+	$RoleID = ForceInt($SortOrder[$i], null);
+	if ($RoleID !== null) {
+		$ExecSql = str_replace(array('//1', '//2'), array($i, $RoleID), $Sql);
+		$Context->Database->Execute($ExecSql, 'AJAX', 'ReorderRoles', 'Failed to reorder roles', 0);
+	}
 }
 echo $SortOrder;
 $Context->Unload();
