4e4f08054bf8dd6db9a8562fd34e36d790b5fa08
wallphone <wallphone@1c12818e-1a40-11de-a108-cd2f117ce590>
2008-02-24 20:37:20 +0000

    http://lussumo.com/bugs/discussion/54/comment-search-results-with-friendly-urls-dont-include-discussion-titles-in-the-urls/
    Added a couple CleanupString() functions in the appropriate places.

--- src/themes/search_results_comments.php
+++ src/themes/search_results_comments.php
@@ -5,11 +5,11 @@ $CommentList .= '<li class="SearchComment'.($Alternate ? ' Alternate' : '').'">
 	<ul>
 		<li class="DiscussionTopic">
 			<span>'.$this->Context->GetDefinition('DiscussionTopic').'</span>
-			<a href="'.GetUrl($this->Context->Configuration, 'comments.php', '', 'DiscussionID', $Comment->DiscussionID).'">'.$Comment->Discussion.'</a>
+			<a href="'.GetUrl($this->Context->Configuration, 'comments.php', '', 'DiscussionID', $Comment->DiscussionID, '', '', CleanupString($Comment->Discussion) .'/').'">'.$Comment->Discussion.'</a>
 		</li>
 		<li class="CommentText">
 			<span>'.$this->Context->GetDefinition('Comment').'</span>
-			<a href="'.GetUrl($this->Context->Configuration, 'comments.php', '', 'DiscussionID', $Comment->DiscussionID, '', 'Focus='.$Comment->CommentID.'#Comment_'.$Comment->CommentID).'">'.HighlightTrimmedString($Comment->Body, $HighlightWords, 300).'</a>
+			<a href="'.GetUrl($this->Context->Configuration, 'comments.php', '', 'DiscussionID', $Comment->DiscussionID, '', 'Focus='.$Comment->CommentID.'#Comment_'.$Comment->CommentID, CleanupString($Comment->Discussion) .'/').'">'.HighlightTrimmedString($Comment->Body, $HighlightWords, 300).'</a>
 		</li>
 		<li class="DiscussionCategory">
 			<span>'.$this->Context->GetDefinition('Category').'</span>
