e2c0d9011f510e38e01dbbc95cb21b7c7172e85c
dinoboff <dinoboff@1c12818e-1a40-11de-a108-cd2f117ce590>
2008-09-07 21:02:36 +0000

    Update version to 1.1.5-rc3

--- trunk/src/appg/version.php
+++ trunk/src/appg/version.php
@@ -19,8 +19,8 @@
  * Previous_vanilla_version: 1.1.4
  */
 define('APPLICATION', 'Vanilla');
-define('FRAMEWORK_VERSION', '1.1.4-rc2');
-define('PEOPLE_VERSION', '1.1.4-rc2');
-define('APPLICATION_VERSION', '1.1.5-rc2');
+define('FRAMEWORK_VERSION', '1.1.4-rc3');
+define('PEOPLE_VERSION', '1.1.4-rc3');
+define('APPLICATION_VERSION', '1.1.5-rc3');
 
 ?>
