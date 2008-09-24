2258ac13bd934d202f9499ff59cb735219c790a4
dinoboff <dinoboff@1c12818e-1a40-11de-a108-cd2f117ce590>
2008-09-24 16:21:18 +0000

    Update versions to 1.1.5a

--- trunk/src/appg/version.php
+++ trunk/src/appg/version.php
@@ -14,13 +14,13 @@
 
 /**
  * Previous version:
- * Previous_framework_version: 1.1.3
- * Previous_people_version: 1.1.3
- * Previous_vanilla_version: 1.1.4
+ * Previous_framework_version: 1.1.4
+ * Previous_people_version: 1.1.4
+ * Previous_vanilla_version: 1.1.5
  */
 define('APPLICATION', 'Vanilla');
-define('FRAMEWORK_VERSION', '1.1.4');
-define('PEOPLE_VERSION', '1.1.4');
-define('APPLICATION_VERSION', '1.1.5');
+define('FRAMEWORK_VERSION', '1.1.5');
+define('PEOPLE_VERSION', '1.1.5');
+define('APPLICATION_VERSION', '1.1.5a');
 
 ?>
