495edb74bddf312197c0726807bdbaeba9185aa9
dinoboff@gmail.com <dinoboff@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2009-11-21 00:01:22 +0000

    update previous version reference

--- trunk/src/appg/version.php
+++ trunk/src/appg/version.php
@@ -14,13 +14,13 @@
 
 /**
  * Previous version:
- * Previous_framework_version: 1.1.6
- * Previous_people_version: 1.1.6
- * Previous_vanilla_version: 1.1.8
+ * Previous_framework_version: 1.1.7
+ * Previous_people_version: 1.1.7
+ * Previous_vanilla_version: 1.1.9
  */
 define('APPLICATION', 'Vanilla');
-define('FRAMEWORK_VERSION', '1.1.7');
-define('PEOPLE_VERSION', '1.1.7');
+define('FRAMEWORK_VERSION', '1.1.8');
+define('PEOPLE_VERSION', '1.1.8');
 define('APPLICATION_VERSION', '1.1.10');
 
 ?>
