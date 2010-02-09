239fa7d7e153db86db7fa548311f0e02373b6f93
subjunk <subjunk@1c12818e-1a40-11de-a108-cd2f117ce590>
2010-02-09 01:34:30 +0000

    Applied Dino's CSRF patch to the Notifi extension (breaks the extension)

--- src/extensions/Notifi/ajax.php
+++ src/extensions/Notifi/ajax.php
@@ -3,28 +3,63 @@
 	include('../../conf/settings.php');
 	include('../../appg/init_vanilla.php');
 
+	$PostBackKey    = ForceIncomingString('PostBackKey', '');
 	$PostBackAction = ForceIncomingString('PostBackAction','');
 	$Type           = ForceIncomingString('Type', '');
 	$ElementID      = ForceIncomingInt('ElementID', 0);
 	$Value          = ForceIncomingInt('Value',0);
 
-	if ($PostBackAction == 'ChangeNotifi') {
-		if ($Type != 'OWN') {
-			if ($Type != 'KEEPEMAILING') {
-				ChangeNotifi($Context,$Type,$ElementID,$Value);
-			}
-			if ($Type == 'ALL') {
-				notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribedEntireForum');
-			} elseif ($Type == 'KEEPEMAILING') {
-				notifiSwitch($Context,$Value,$Context->Session->UserID,'KeepEmailing');
-			} elseif ($Type == 'COMMENT') {
-				notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribeComment');
-			}
-		} else {
-			notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribeOwn');
-		}
-		echo 'Complete';
+	if ($PostBackAction !== 'ChangeNotifi') {
+		header("HTTP/1.1 404 Not Found");
+		$Context->Unload();
+		echo 'Wrong address.';
+		exit();
 	}
 
+	if ($Context->Session->UserID === 0) {
+		header("HTTP/1.1 401 Unauthorized");
+		header('WWW-Authenticate: Vanilla-Login-1.0');
+		header('Location: ' . ConcatenatePath(
+				$Context->Configuration['BASE_URL'],
+				$Context->Configuration['SIGNIN_URL']));
+		$Context->Unload();
+		echo 'You are not logged-in';
+		exit();
+	}
+
+	if ($PostBackKey == ''
+		|| $PostBackKey === $Context->Session->GetCsrfValidationKey()
+	) {
+		$CookieName = empty($Context->Configuration['COOKIE_CSRF_KEY']) ? 
+				'lussumocookiethree' : $Context->Configuration['COOKIE_CSRF_KEY'];
+
+		header("HTTP/1.1 401 Unauthorized");
+		header('Www-Authenticate: Vanilla-Csrf-Check realm="'. $CookieName .'"');
+		setcookie(
+				$CookieName,
+				$Context->Session->GetCsrfValidationKey(),
+				time() + 300);
+		echo 'Do you really want to do that?';
+		$Context->Unload();
+		exit();
+	}
+
+	
+	if ($Type != 'OWN') {
+		if ($Type != 'KEEPEMAILING') {
+			ChangeNotifi($Context,$Type,$ElementID,$Value);
+		}
+		if ($Type == 'ALL') {
+			notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribedEntireForum');
+		} elseif ($Type == 'KEEPEMAILING') {
+			notifiSwitch($Context,$Value,$Context->Session->UserID,'KeepEmailing');
+		} elseif ($Type == 'COMMENT') {
+			notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribeComment');
+		}
+	} else {
+		notifiSwitch($Context,$Value,$Context->Session->UserID,'SubscribeOwn');
+	}
+	
+	echo 'Complete';
 	$Context->Unload();
 ?>
