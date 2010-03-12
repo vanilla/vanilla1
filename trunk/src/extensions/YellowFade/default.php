3f422364cba4ea542f8c4f6e391b9dda8b4b0cb0
dinoboff@gmail.com <dinoboff@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2010-03-12 02:03:58 +0000

    Port Yellow Fade to jQuery UI's Highlight effect.
    There no more pollution of the global scope, It doesn't change the the background colour of the element and is easier to customised (you on need to set the highlight colour - yellow by default).

--- trunk/src/extensions/YellowFade/default.php
+++ trunk/src/extensions/YellowFade/default.php
@@ -1,14 +1,37 @@
 <?php
 /*
 Extension Name: Yellow Fade
-Extension Url: http://michaelraichelson.com/hacks/vanilla/
+Extension Url: http://vanillaforums.org/addon/136/yellow-fade
 Description: Adds the YellowFade effect on some stuff.
-Version: 0.1
+Version: 0.2
 Author: Michael Raichelson
 Author Url: http://michaelraichelson.com/
-*/
+ *
+ * Copyright 2005 Michael Raichelson <http://michaelraichelson.com/hacks/vanilla/>
+ * Copyright 2006, 2007 Mark O'Sullivan <mark@vanillaforums.com>
+ * Copyright 2010 Damien Lebrun <dinoboff@gmail.com>
+ *
+ */
 
-if (in_array($Context->SelfUrl, array("index.php", "categories.php", "comments.php", "search.php", "post.php", "account.php", "settings.php"))){
-    $Head->AddScript('extensions/YellowFade/functions.js');
+if (in_array(
+		$Context->SelfUrl,
+		array(
+			"index.php",
+			"categories.php",
+			"comments.php",
+			"search.php",
+			"post.php",
+			"account.php",
+			"settings.php"
+			)
+		)
+){
+	if (version_compare(APPLICATION_VERSION, '1.2-dev') < 0) {
+		$Head->AddScript('extensions/YellowFade/functions.js', '~');
+	} else {
+		$Head->AddScript('js/jquery.js', '~', 190);
+		$Head->AddScript('js/jquery-ui/jquery.effects.core.js', '~', 191);
+		$Head->AddScript('js/jquery-ui/jquery.effects.highlight.js', '~', 192);
+		$Head->AddScript('extensions/YellowFade/jquery.yellowfade.js', '~', 260);
+	}
 }
-?>
