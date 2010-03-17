276e6d8fc062fa215c8935b7a01b02ed780337a6
dinoboff@gmail.com <dinoboff@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2010-03-17 22:01:10 +0000

    Add Ip History to build script after some minor changes

--- src/extensions/IpHistory/default.php
+++ src/extensions/IpHistory/default.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Extension Name: IP History
-Extension Url: http://lussumo.com/docs/
+Extension Url: http://vanillaforums.org/addon/10/ip-history
 Description: Adds a complete summary of all IP addresses a user has ever had (and who has shared those IP addresses) to their profile. Note: you must have IP logging turned on in order to record and display this data.
-Version: 1.0.1
+Version: 1.0.2
 Author: Mark O'Sullivan
 Author Url: http://www.markosullivan.ca/
 
@@ -23,58 +23,66 @@ $Configuration['LOG_ALL_IPS'] = '1';
 */
 if (!defined('IN_VANILLA')) exit();
 
-$Context->Dictionary["CommentPostedFromX"] = "Comment posted from //1";
-$Context->Dictionary["IpHistory"] = "IP history";
-$Context->Dictionary["XTime"] = "//1 time";
-$Context->Dictionary["XTimes"] = "//1 times";
-$Context->Dictionary["IpAlsoUsedBy"] = "This IP address has also been used by the following users:";
-$Context->Dictionary["IpNotShared"] = "This IP address has not been shared by any other users.";
-$Context->Dictionary["NoIps"] = "This user does not appear to have logged any IP addresses.";
+$Context->SetDefinition("CommentPostedFromX", "Comment posted from //1");
+$Context->SetDefinition("IpHistory", "IP history");
+$Context->SetDefinition("XTime", "//1 time");
+$Context->SetDefinition("XTimes", "//1 times");
+$Context->SetDefinition("IpAlsoUsedBy", "This IP address has also been used by the following users:");
+$Context->SetDefinition("IpNotShared", "This IP address has not been shared by any other users.");
+$Context->SetDefinition("NoIps", "This user does not appear to have logged any IP addresses.");
 
 if ($Context->SelfUrl == "account.php") {
-   
-   // Displays a user's IP history (for admins only)
-   class IpHistory extends Control {
-      var $History;		// The user's IP history data
-      
-      function IpHistory(&$Context, &$UserManager, $UserID) {
-			$this->Name = "IpHistory";
-         $this->PostBackAction = ForceIncomingString("PostBackAction", "");
-			$this->Control($Context);
-         $this->History = false;
-         if ($this->Context->Session->User) {
-            if ($this->Context->Session->User->Permission("PERMISSION_IP_ADDRESSES_VISIBLE") && $this->PostBackAction == "") $this->History = $UserManager->GetIpHistory($UserID);
-         }
-      }
-      
-      function Render() {
-         if ($this->History && $this->PostBackAction == "") {
-            include($this->Context->Configuration["EXTENSIONS_PATH"]."IpHistory/account_ip_history.php");
-         }
-      }
-   }
 
-   // Don't reload objects if you don't need to (ie. If another extension has already loaded it)
-   if (!@$UserManager) $UserManager = $Context->ObjectFactory->NewContextObject($Context, "UserManager");
-   $AccountUserID = ForceIncomingInt("u", $Context->Session->UserID);
-   if (!@$AccountUser) $AccountUser = $UserManager->GetUserById($AccountUserID);
-  	$Page->AddRenderControl($Context->ObjectFactory->NewContextObject($Context, "IpHistory", $UserManager, $AccountUserID), $Configuration["CONTROL_POSITION_BODY_ITEM"]);
+	// Displays a user's IP history (for admins only)
+	class IpHistory extends Control {
+		var $History;		// The user's IP history data
+
+		function IpHistory(&$Context, &$UserManager, $UserID) {
+			$this->Name = "IpHistory";
+			$this->PostBackAction = ForceIncomingString("PostBackAction", "");
+			$this->Control($Context);
+			$this->History = false;
+			if ($this->Context->Session->User) {
+				if ($this->Context->Session->User->Permission("PERMISSION_IP_ADDRESSES_VISIBLE") && $this->PostBackAction == "") $this->History = $UserManager->GetIpHistory($UserID);
+			}
+		}
+
+		function Render() {
+			if ($this->History && $this->PostBackAction == "") {
+				$DefaultThemeDir = dirname(__FILE__) . '/';
+				if (version_compare(APPLICATION_VERSION, '1.2-dev', '<')) {
+					$ThemeFile = $DefaultThemeDir . 'account_ip_history.php';
+				} else {
+					$ThemeFile = ThemeFilePath(
+							$this->Context->Configuration,
+							'account_ip_history.php',
+							$DefaultThemeDir);
+				}
+				include($ThemeFile);
+			}
+		}
+	}
+
+	// Don't reload objects if you don't need to (ie. If another extension has already loaded it)
+	if (!@$UserManager) $UserManager = $Context->ObjectFactory->NewContextObject($Context, "UserManager");
+	$AccountUserID = ForceIncomingInt("u", $Context->Session->UserID);
+	if (!@$AccountUser) $AccountUser = $UserManager->GetUserById($AccountUserID);
+	$Page->AddRenderControl($Context->ObjectFactory->NewContextObject($Context, "IpHistory", $UserManager, $AccountUserID), $Configuration["CONTROL_POSITION_BODY_ITEM"]);
 }
 
 // Displays the user's ip address next to his/her comment if the viewing user is an administrator
 if ($Context->SelfUrl == "comments.php") {
 	function CommentGrid_ShowIPAddress(&$CommentGrid) {
-      if ($CommentGrid->Context->Session->UserID > 0) {
-         if ($CommentGrid->Context->Session->User->Permission("PERMISSION_IP_ADDRESSES_VISIBLE")) {
+		if ($CommentGrid->Context->Session->UserID > 0) {
+			if ($CommentGrid->Context->Session->User->Permission("PERMISSION_IP_ADDRESSES_VISIBLE")) {
 				$Comment = $CommentGrid->DelegateParameters["Comment"];
 				$CommentList = &$CommentGrid->DelegateParameters["CommentList"];
 				$CommentList .= str_replace("//1", $Comment->RemoteIp, $CommentGrid->Context->GetDefinition("CommentPostedFromX"));
 			}
 		}
 	}
-	
+
 	$Context->AddToDelegate("CommentGrid",
-		"PreCommentOptionsRender",
-		"CommentGrid_ShowIPAddress");	
+			"PreCommentOptionsRender",
+			"CommentGrid_ShowIPAddress");
 }
-?>
