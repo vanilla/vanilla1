c34ed201b2e929700c4b76e327c7f979f9c88d7a
dinoboff@gmail.com <dinoboff@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2010-03-17 21:23:36 +0000

    Add New Applicants to build

--- trunk/src/extensions/NewApplicants/default.php
+++ trunk/src/extensions/NewApplicants/default.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Extension Name: New Applicants
-Extension Url: http://lussumo.com/addons/
+Extension Url: http://vanillaforums.org/addon/45/new-applicants
 Description: Places notification above discussion list for users with sufficient priviledges that there are new applicants seeking membership approval.
 Version: 1.3
 Author: Mark O'Sullivan
@@ -15,11 +15,10 @@ $Context->SetDefinition('NewApplicants_ClickHereToReviewApplicants', 'Click here
 
 // Attach to the user account being viewed if there is no postback action
 if ($Context->SelfUrl == 'index.php' && $Context->Session->UserID > 0 && $Context->Session->User->Permission('PERMISSION_APPROVE_APPLICANTS')) {
-   $UserManager = $Context->ObjectFactory->NewContextObject($Context, 'UserManager');
-   $ApplicantCount = $UserManager->GetApplicantCount();
-   if ($ApplicantCount > 0) {
+	$UserManager = $Context->ObjectFactory->NewContextObject($Context, 'UserManager');
+	$ApplicantCount = $UserManager->GetApplicantCount();
+	if ($ApplicantCount > 0) {
 		$NoticeCollector->AddNotice(($ApplicantCount != 1 ? str_replace('\\1', $ApplicantCount, $Context->GetDefinition('NewApplicants_XNewApplicants')) : $Context->GetDefinition('NewApplicants_OneNewApplicant')).'
          <a href="'.GetUrl($Context->Configuration, 'settings.php', '', '', '', '', 'PostBackAction=Applicants').'">'.$Context->GetDefinition('NewApplicants_ClickHereToReviewApplicants').'</a>');
-   }
+	}
 }
-?>
