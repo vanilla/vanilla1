fe2a72b8e14f73e990b3a17d750e7019e454541e
subjunk@gmail.com <subjunk@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2011-04-26 23:34:56 +0000

    Released HtmlFormatter 2.5

---
+++ tags/HtmlFormatter-2.5/functions.js
@@ -0,0 +1,25 @@
+(function($){
+    $.HtmlFormatter = {
+        margin: 290,
+
+        getDesiredWidth: function() {
+            var innerWidth = window.innerWidth ||
+                document.documentElement.clientWidth;
+            return innerWidth - $.HtmlFormatter.margin;
+        },
+
+        changeDimensions: function() {
+            var $img = $('img.InlineImage'),
+                desiredWidth = $.HtmlFormatter.getDesiredWidth();
+
+            $img.width('auto');
+            if ($img.width() > desiredWidth) {
+                $img.width(desiredWidth);
+            }
+        }
+    };
+
+    $(window).bind('load resize', $.HtmlFormatter.changeDimensions);
+
+
+})(jQuery.noConflict());
