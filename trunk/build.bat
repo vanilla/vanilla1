cb9350c14a5f178d2080760985123deb60499f6d
subjunk@gmail.com <subjunk@gmail.com@1c12818e-1a40-11de-a108-cd2f117ce590>
2011-04-26 22:41:53 +0000

    Build supports portable Ant and JDK now if they are in the tools folder, in folders called "ant" and "jdk" (so now they don't have to be installed)

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,11 +1,24 @@
 :checkAntHome
 if not defined ANT_HOME goto noAntHome
 set ANT_EXEC=%ANT_HOME%\bin\ant.bat
-goto build
+goto checkJavaHome
 
 :noAntHome
-echo WARNING: You have not set the ANT_HOME environment variable. Ant is require to build vanilla.
-set ANT_EXEC=ant
+echo WARNING: You have not set the ANT_HOME environment variable. Ant is required to build Vanilla.
+echo Checking for Ant in tools folder...
+set ANT_HOME=%CD%\tools\ant
+set ANT_EXEC=%ANT_HOME%\bin\ant.bat
+goto checkJavaHome
+
+:checkJavaHome
+if not defined JAVA_HOME goto noJavaHome
+goto build
+
+:noJavaHome
+echo WARNING: You have not set the JAVA_HOME environment variable. JDK is required to build Vanilla.
+echo Checking for JDK in tools folder...
+set JAVA_HOME=%CD%\tools\jdk
+goto build
 
 :build
 %ANT_EXEC% update && %ANT_EXEC%
