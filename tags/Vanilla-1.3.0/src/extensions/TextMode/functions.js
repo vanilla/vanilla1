2f1abc912dfe0843415e3933fd2a7920a0b6ca5a
SubJunk <SubJunk@1c12818e-1a40-11de-a108-cd2f117ce590>
2012-02-16 20:38:57 +0000

    Revert 1.3.0 release temporarily

--- tags/Vanilla-1.3.0/src/extensions/TextMode/functions.js
+++
@@ -1,7 +0,0 @@
-function SwitchTextMode(AjaxUrl, Switch, PostBackKey) {
-   var dm = new DataManager();
-   dm.Param = 'HtmlOn';
-   dm.RequestFailedEvent = HandleFailure;
-   dm.RequestCompleteEvent = RefreshPageWhenAjaxComplete;
-   dm.LoadData(AjaxUrl+"?Type=HtmlOn&PostBackKey="+PostBackKey+"&Switch="+Switch);
-}
